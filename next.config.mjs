01c98585bf046d6ae414900d81a6c5b580ecc532
rosyid <rajamasker@gmail.com>
2024-01-26 06:32:19 +0900

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   distDir: 'dist',
   basePath: '/nteractive-rating-component',
-  // images: {
-  //   unoptimized: true
-  // }
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
